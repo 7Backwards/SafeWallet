3a3e7316f2b5f0d42e47c4d7bed4c5e68fa583a1
Gonçalo Neves <neves.goncaloscp9@gmail.com>
2024-01-21 23:09:19 +0000

    Moved duplicated code to the appManager utils, making the app cleaner

--- SafeWallet/SafeWallet/ViewModels/CardDetailsViewModel.swift
+++ SafeWallet/SafeWallet/ViewModels/CardDetailsViewModel.swift
@@ -16,32 +16,11 @@ class CardDetailsViewModel: ObservableObject {
     }
 
     func formatCardNumber(_ number: String) -> String {
-        // First, filter out any non-numeric characters
-        let filtered = number.filter { "0123456789".contains($0) }
-        
-        // Then, limit to the first 16 characters
-        let trimmed = String(filtered.prefix(16))
-        
-        // Finally, add a space after every 4 digits
-        return stride(from: 0, to: trimmed.count, by: 4).map {
-            let start = trimmed.index(trimmed.startIndex, offsetBy: $0)
-            let end = trimmed.index(start, offsetBy: 4, limitedBy: trimmed.endIndex) ?? trimmed.endIndex
-            return String(trimmed[start..<end])
-        }.joined(separator: " ")
+        appManager.utils.formatCardNumber(number)
     }
     
     func getCardIssuerImage(cardNumber: String) -> Image? {
-        let formattedNumber = cardNumber.replacingOccurrences(of: " ", with: "")
-        
-        if formattedNumber.hasPrefix("4") {
-            return Image("visa")
-        } else if formattedNumber.range(of: "^(51|52|53|54|55|2221|222[2-9]|22[3-9]\\d|2[3-6]\\d\\d|27[01]\\d|2720)", options: .regularExpression) != nil {
-            return Image("mastercard")
-        } else if formattedNumber.range(of: "^(34|37)", options: .regularExpression) != nil {
-            return Image("american-express")
-        } else {
-            return nil
-        }
+        appManager.utils.getCardIssuerImage(cardNumber: cardNumber)
     }
     
     func getCardBackgroundOpacity() -> Double {
--- SafeWallet/SafeWallet/Views/ColorCarouselView.swift
+++ SafeWallet/SafeWallet/Views/ColorCarouselView.swift
@@ -10,19 +10,16 @@ import SwiftUI
 struct ColorCarouselView: View {
     @Binding var cardColor: String
     @StateObject var viewModel: ColorCarouselViewModel
-    private let colorCircleSize: CGFloat = 40
     
-    let colors: [String] = ["red", "orange" , "purple", "systemBackground", "gray","green"]
-
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 15) {
-                    ForEach(colors, id: \.self) { colorName in
+                    ForEach(viewModel.appManager.constants.colors, id: \.self) { colorName in
                         let color = Color(wordName: colorName)?.opacity(viewModel.getCardBackgroundOpacity()) ?? .clear
                         Circle()
                             .fill(color)
-                            .frame(width: colorCircleSize, height: colorCircleSize)
+                            .frame(width: viewModel.appManager.constants.colorCircleSize, height: viewModel.appManager.constants.colorCircleSize)
                             .overlay(
                                 Circle()
                                     .stroke(Color.clear)
@@ -37,9 +34,9 @@ struct ColorCarouselView: View {
                     }
                 }
                 .frame(width: geometry.size.width)
-                .frame(height: colorCircleSize * 1.4)
+                .frame(height: viewModel.appManager.constants.colorCircleSize * 1.4)
             }
         }
-        .frame(height: colorCircleSize * 1.4)
+        .frame(height: viewModel.appManager.constants.colorCircleSize * 1.4)
     }
 }
