ae31b9116559dd148d56f34e0c2f1725aa5380cb
Gonçalo Neves <goncalo.neves@8x8.com>
2024-01-23 00:23:27 +0000

    Added verification fields for input fields on add card

--- SafeWallet/SafeWallet/ViewModels/AddCardViewModel.swift
+++ SafeWallet/SafeWallet/ViewModels/AddCardViewModel.swift
@@ -9,6 +9,12 @@ import Foundation
 import CoreData
 import SwiftUI
 
+enum AddCardErrorType: Error {
+    case invalidDate
+    case savingError
+    case shortCardNumber
+}
+
 class AddCardViewModel: ObservableObject {
     @Published var selectedColor: Color?
     @Published var appManager: AppManager
@@ -17,10 +23,32 @@ class AddCardViewModel: ObservableObject {
         self.appManager = appManager
     }
     
-    func addCard(cardName: String, cardNumber: String, expiryDate: String, cvvCode: String, cardColor: String) {
+    func addCard(cardName: String, cardNumber: String, expiryDate: String, cvvCode: String, cardColor: String, completion: @escaping (Result<Void, AddCardErrorType>) -> Void) {
         guard !cardName.isEmpty, !cardNumber.isEmpty, !expiryDate.isEmpty, !cvvCode.isEmpty, !cardColor.isEmpty else { return }
+        
+        if cardNumber.count != 16 {
+            completion(.failure(.shortCardNumber))
+            return
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/yy"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.isLenient = false
 
-        appManager.actionManager.doAction(action: .addCard(cardName: cardName, cardNumber: cardNumber, expiryDate: expiryDate, cvvCode: cvvCode, cardColor: cardColor))
+        if let date = dateFormatter.date(from: expiryDate) {
+            if Calendar.current.compare(date, to: Date(), toGranularity: .month) == .orderedDescending {
+                appManager.actionManager.doAction(action: .addCard(cardName: cardName, cardNumber: cardNumber, expiryDate: expiryDate, cvvCode: cvvCode, cardColor: cardColor)) { result in
+                    completion(result ? .success(()) : .failure(.savingError))
+                }
+            } else {
+                completion(.failure(.invalidDate))
+                print("The expiry date must be later than the current date.")
+            }
+        } else {
+            completion(.failure(.invalidDate))
+            print("Invalid date format. Please enter the expiry date in mm/yy format.")
+        }
     }
 }
 
